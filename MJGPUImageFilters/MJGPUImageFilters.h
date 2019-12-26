ba6bd3f25a65c2afb412904eddfcb7a80b6b1b99
Mateusz Madej <madejmt@gmail.com>
2019-12-26 14:54:25 +0100

    Initial Commit

---
+++ MJGPUImageFilters/MJGPUImageFilters.h
@@ -0,0 +1,19 @@
+//
+//  MJGPUImageFilters.h
+//  MJGPUImageFilters
+//
+//  Created by Mateusz Madej on 26/12/2019.
+//  Copyright © 2019 mejdej. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MJGPUImageFilters.
+FOUNDATION_EXPORT double MJGPUImageFiltersVersionNumber;
+
+//! Project version string for MJGPUImageFilters.
+FOUNDATION_EXPORT const unsigned char MJGPUImageFiltersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJGPUImageFilters/PublicHeader.h>
+
+
